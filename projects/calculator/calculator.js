ece82cd15141a357670608d7cd9775fb80da94d3
Doston Nabotov <96492656+dostonnabotov@users.noreply.github.com>
2022-05-07 23:24:22 +0300

    Algorithmization added

--- projects/calculator/calculator.js
+++
@@ -1,7 +0,0 @@
-// create variables
-
-const num1 = parseInt(prompt('Enter the first number: '));
-const num2 = parseInt(prompt('Enter the second number: '));
-const sum = num1 + num2;
-
-document.write(`${num1} + ${num2} =  ${sum}`);
