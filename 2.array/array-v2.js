5343770a2ddd6fc7eda38101f4e7dc42dd42d40e
Doston Nabotov <96492656+dostonnabotov@users.noreply.github.com>
2022-05-17 01:40:58 +0300

    array-v2 updated

--- 2.array/array-v2.js
+++ 2.array/array-v2.js
@@ -1,50 +1,99 @@
 /**************************\
     ARRAY - Intermediate
 \**************************/
+/*--------------
+Layout: 
+    1. Map
+    2. Filter
+    3. Find
+    4. Some
+    5. Every
+    5. Reduce
+    6. forEach
+    7. Includes
+----------------*/
+
 const items = [
-  { name: "Bike", price: 300 },
   { name: "TV", price: 200 },
-  { name: "Computer", price: 150 },
+  { name: "Car", price: 900 },
+  { name: "Book", price: 15 },
+  { name: "Bike", price: 300 },
   { name: "Phone", price: 50 },
-  { name: "Book", price: 5 },
-  { name: "Car", price: 1000 },
+  { name: "Computer", price: 150 },
 ];
 
-// Map through items
-const itemNames = items.map((item) => {
-  return item.price;
-});
+// Map
+const mapItems = items.map((item) => item.price * 2);
+// expected output: [ 400, 1800, 30, 600, 100, 300 ]
 
-// Find an exact item
-const foundItems = items.find((item) => {
-  return item.name === "Book";
-});
+// Filter
+const filterItems = items.filter((item) => item.price <= 150);
+// expected output:
+// [
+//   { name: "Book", price: 15 },
+//   { name: "Phone", price: 50 },
+//   { name: "Computer", price: 150 },
+// ]
 
-// Check if there is any item which is less than $100,
-// If yes, return 'true'. Else, return 'false'
-const someInexpensiveItems = items.some((item) => {
-  return item.price <= 100;
-});
+// Find
+const findItems = items.find((item) => item.name === "Book");
+// expected output: { name: 'Book', price: 15 }
 
-// Check if  all items are less than $100,
-// If yes, return 'true'. Else, return 'false'
-const everyInexpensiveItems = items.every((item) => {
-  return item.price <= 100;
-});
+// Some
+const someItems = items.some((item) => item.price <= 100);
+// expected output: true
 
-// Return the total value/price
-const total = items.reduce((currentTotal, item) => {
+// Every
+const everyItems = items.every((item) => item.price <= 100);
+// expected output: false
+
+// Sort - ascending order
+const sortItems_ascend = items.sort((a, b) => a.price - b.price);
+// expected output:
+// [
+//   { name: "Book", price: 15 },
+//   { name: "Phone", price: 50 },
+//   { name: "Computer", price: 150 },
+//   { name: "TV", price: 200 },
+//   { name: "Bike", price: 300 },
+//   { name: "Car", price: 900 },
+// ]
+
+// Sort - ascending order
+const sortItems_descend = items.sort((a, b) => b.price - a.price);
+// expected output:
+// [
+//   { name: "Car", price: 900 },
+//   { name: "Bike", price: 300 },
+//   { name: "TV", price: 200 },
+//   { name: "Computer", price: 150 },
+//   { name: "Phone", price: 50 },
+//   { name: "Book", price: 15 },
+// ]
+
+// Reduce
+const reduceItems = items.reduce((currentTotal, item) => {
   return item.price + currentTotal;
 }, 0);
+// expected output: 1615
 
-// Loop through items
-items.forEach((item) => {
-  console.log(item.name);
+// forEach
+items.forEach((item, index) => {
+  // console.log(`${item.name} in index of ${index} costs $${item.price}`);
 });
+// expected output:
+// Car in index of 0 costs 900
+// Bike in index of 1 costs 300
+// TV in index of 2 costs 200
+// e.t.c...
 
-// Feel free to uncomment and check it in the console
-// console.log(itemNames);
-// console.log(foundItems);
-// console.log(someInexpensiveItems);
-// console.log(everyInexpensiveItems);
-// console.log(total);
+// Includes
+const pets = ["car", "bike", "laptop"];
+pets.includes("car");
+// expected output: true
+
+pets.includes("computer");
+// expected output: false
+
+pets.includes("Bike");
+// expected output: false
